7fbc236000752dba6d6fc4a6cc6f0885d343f5c5
Hugo Rodrigues <me@hugorodrigues.net>
2017-11-06 14:41:25 +0000

    [IMP] Using company_partner_ud

--- hugorodrigues_install/models/hugo_rodrigues.py
+++ hugorodrigues_install/models/hugo_rodrigues.py
@@ -35,13 +35,12 @@ class ResConfigSettings(models.AbstractModel):
             'email': 'me@hugorodrigues.net',
             })
 
+        logger.info('Setting company data')
         company = self.env.ref('base.main_company')
-        partner = company.partner_id
         company.write({
             'report_header': 'Geek since 1995',
             'social_twitter': 'hmatosrodrigues',
             'social_github': 'hmrodrigues'
             })
-        logger.info('Setting partner data')
-        partner.write(company_values)
+        company.partner_id.write(company_values)
         return True
