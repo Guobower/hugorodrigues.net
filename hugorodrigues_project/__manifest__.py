0d05e89f32dde8f7772a87c0a38f6469ecd92c25
Hugo Rodrigues <me@hugorodrigues.net>
2018-03-25 23:28:49 +0100

    [ADD] hugorodrigues_project
    Auto adds stages to new projects

--- hugorodrigues_project/__manifest__.py
+++ hugorodrigues_project/__manifest__.py
@@ -1,17 +1,12 @@
 # coding: utf-8
 # pylint: disable=missing-docstring
 {
-    'name': 'Hugo Rodrigues Install',
+    'name': 'Hugo Rodrigues Project',
     'version': '11.0.1.0.0',
     'author': 'Hugo Rodrigues',
     'website': 'https://hugorodrigues.net',
     'license': 'Other OSI approved licence',
     'depends': [
-        'hugorodrigues_website',
-        'hugorodrigues_blog',
-        'hugorodrigues_project',
-        'url_shorter',
         'project',
-        'note_extended'
     ],
 }
---
+++ hugorodrigues_project/models/project.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+"""Custom settings for projects and taks"""
+from odoo import models, api
+
+
+class Project(models.Model):
+    """Project ext"""
+    _inherit = 'project.project'
+
+    @api.model
+    def create(self, vals):
+        """Auto add stages to project"""
+        project = super(Project, self).create(vals)
+        stages = self.env['project.task.type'].search([])
+        stages.write({'project_ids': [(4, project.id)]})
+        return project
+
+
+class TaskType(models.Model):
+    """TaskType (stage) ext"""
+    _inherit = 'project.task.type'
+
+    @api.model
+    def create(self, vals):
+        """Add stage to all projects"""
+        stage = super(TaskType, self).create(vals)
+        projects = self.env['project.project'].search([])
+        stage.write({'project_ids': [(6, 0, projects.ids)]})
+        return stage
