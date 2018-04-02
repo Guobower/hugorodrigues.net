68d6ad9dd89d592defd3b53cb7dd92bd58d84bcf
Hugo Rodrigues <me@hugorodrigues.net>
2018-04-02 19:41:57 +0100

    [IMP] Improved general layouts

--- hugorodrigues_blog/static/src/js/navbar.js
+++ hugorodrigues_blog/static/src/js/navbar.js
@@ -1,13 +1,21 @@
 odoo.define('hugorodrigues_blog.navbar', function(require) {
     "use strict";
 
-    $(window).scroll(function() {
-        var st = $(this).scrollTop();
-        var navbar = $('.navbar')
-        if (st == 0) {
-            navbar.addClass('pg-on-top');
+    $(document).ready(function() {
+        var navbar = $('.navbar');
+        if ($('#wrapwrap').height() > $(window).height()) {
+            $(window).scroll(function() {
+                var st = $(this).scrollTop();
+                if (st == 0) {
+                    navbar.addClass('pg-on-top');
+                } else {
+                    navbar.removeClass('pg-on-top');
+                }
+            });
         } else {
+            navbar.addClass('notransition');
             navbar.removeClass('pg-on-top');
+            $('main').addClass('noscroll');
         }
     });
 });
