d5427143aae898d6e0a5262d026a41fb32f15a3c
Hugo Rodrigues <me@hugorodrigues.net>
2018-06-21 16:03:45 +0100

    [IMP] Improved wesite analytics models

--- hugorodrigues_website/models/website_analytics.py
+++ hugorodrigues_website/models/website_analytics.py
@@ -1,18 +1,64 @@
 # coding: utf-8
 """Used to set track visitors"""
-import logging
+
+try:
+    from geoip import geolite2
+    geolite2.lookup("127.0.0.1")  # Check if lookup is available
+except (ImportError, RuntimeError):
+    geolite2 = False
 
 from odoo import models, api, fields
 
 
-class WebsiteAnalytics(models.Model):
+class WebsiteAnalyticsUser(models.Model):
+    """
+    Used to set track visitors.
+    """
+    _name = "website.analytics.user"
+
+    user_id = fields.Many2one(comodel_name="res.users", string="User")
+    ip = fields.Char()
+    country_id = fields.Many2one(compute="_compute_country",
+                                 comodel_name="res.country", string="Country",
+                                 store=True)
+
+    browser_ids = fields.Many2many(comodel_name="website.analytics.browser",
+                                   string="Browsers")
+
+    @api.depends("ip")
+    def _compute_country(self):
+        ResCountry = self.env["res.country"].sudo()
+        for user in self:
+            if geolite2:
+                match = geolite2.lookup(user.ip)
+                if match:
+                    country = ResCountry.search([("code", "=",
+                                                  match.country)], limit=1)
+                    if country:
+                        user.country_id = country.id
+                        continue
+
+
+class WebsiteAnalyticsVisit(models.Model):
     """
     Used to set track visitors.
     This module will not have views we since will use Grafana to display
     statitics"""
-    _name = 'website.analytics'
+    _name = "website.analytics.visit"
 
-    url = fields.Char()
-    page_title = fields.Char()
-    source_ip = fields.Char()
-    source_page = fields.Char()
+    analytics_user_id = fields.Many2one(comodel_name="website_analytics.user",
+                                        required=True, string="Visitor")
+
+    page = fields.Char()
+    source = fields.Char()
+
+
+class WebsiteAnalyticsBrowsers(models.Model):
+    """
+    Used to map browsers
+    """
+    _name = "website.analytics.browser"
+
+    name = fields.Char(required=True)
+    version = fields.Char()
+    user_agent = fields.Char(required=True)
